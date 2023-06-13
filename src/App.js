3d16540ea8f5a0de0af9b4db9d178564e4a0a41d
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-06-13 17:12:57 +0545

    handle searches with lazy queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Characater from "./pages/Characater";
 import CharactersList from "./pages/CharactersList";
+import Search from "./pages/Search";
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<CharactersList />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/:id" element={<Characater />} />
+
       </Routes>
     </div>
   );
